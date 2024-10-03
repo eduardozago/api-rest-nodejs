4c614c0dedf89e2635da3037502141a515938d05
Eduardo Zago <zagoe22@gmail.com>
2024-10-03 08:49:08 -0300

    Routes: Implement route plugins setup

--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,11 @@ import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Some title here',
-      amount: 4560,
-    })
-    .returning('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
